a4048e7bb8ba7b5947f99298b1edbbcca50bc4e1
artemgassan <artemgassan@ya.ru>
2025-01-03 14:45:40 +0300

    feat: finish calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -330,8 +330,11 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce(
+    (sum, currentValue) => sum + (currentValue[0] - currentValue[1]),
+    0
+  );
 }
 
 /**
